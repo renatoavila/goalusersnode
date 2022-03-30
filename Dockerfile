3da0598d0d6420f9f781b56f8e33dbeb30775078
buslui <65923836+buslui@users.noreply.github.com>
2022-03-30 08:54:42 -0300

    mudancas na conexao

--- Dockerfile
+++ Dockerfile
@@ -17,3 +17,13 @@ COPY . .
 
 EXPOSE 3003
 CMD [ "npm", "start" ]
+
+###############################################################################
+#  I M P O R T A N T E
+#
+#  nao esquecer de especificar tag para a imagem quando executar o docker build
+#
+#  docker build . -t goalusers-node
+#
+#  pois o nome da imagem sera utilizado como referencia no docker-compose
+###############################################################################
--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const { index: listarTodos, store: adicionar, find: listarPorId, update: atualizar, exclude: remover } = require("./controller/user");
-//const {homePage} =require("../../GoalAngular/pages/index.ts")
+
 const app = require("./app");
-
 var ip = require("ip");
-console.dir ( ip.address() );
+const cors = require('cors');
 
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
+console.dir ( ip.address() );
 const port = 3003;
 
-console.log(`Goal starting ...`)
+console.log(`Goal Users starting ...`)
 
 app.listen(port, () => {
     console.log(`Goal Users listening at ${ip.address()}:${port}`)
