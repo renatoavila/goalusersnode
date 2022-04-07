752e391b4a1bda49072780901e2b8e83e2c8c8a6
buslui <65923836+buslui@users.noreply.github.com>
2022-04-06 21:20:27 -0300

    ajuste do cors

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 app.use((req, res, next) => {
 	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Origin", "http://144.22.210.64:4200");
 	//Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     app.use(cors());
